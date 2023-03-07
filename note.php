a86014fa8908ad61b5753ea323f45b10c27e1ce0
IwatsukaYura <iwatsukayura@gmail.com>
2023-03-07 14:52:43 +0900

    記録のページを作成しました。

--- note.php
+++ note.php
@@ -1,1 +1,29 @@
+<?php
+require('function_library.php');
+session_start();
 
+if(isset($_SESSION['id']) && $_SESSION['name']){
+  $id = $_SESSION['id'];
+  $name = $_SESSION['name'];
+}else{
+  header('Location: login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <a href="home.php"><img src="./images/logo.png" alt="ロゴの写真" class="logo-img"></a>
+    <h1>Work Out Provider</h1>
+  </header>
+
+</body>
+</html>
